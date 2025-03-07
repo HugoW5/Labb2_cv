4e5a5531176d5fa50f6bb4d783588b4379fd7166
HugoW5 <hugo05.wakt@gmail.com>
2025-03-07 10:19:59 +0100

    css - print

--- script.js
+++ script.js
@@ -29,4 +29,64 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
+const sequense = "1337";
+let sequenseIndex = 0;
+document.body.addEventListener("keydown", (e) => {
+  if (e.key == sequense[sequenseIndex]) {
+    console.log(e.key);
+    sequenseIndex++;
+    if (sequenseIndex == 4) {
+      alert("Du hittade påskäggget");
+      sequenseIndex = 0;
+    }
+  } else {
+    sequenseIndex = 0;
+  }
+});
 
+document.getElementById("footer_heading").addEventListener("click", () => {
+  document.body.style.backgroundColor = "green";
+});
+
+async function templateResume() {
+  try {
+    await fetch("./resume.json")
+      .then((res) => res.json())
+      .then((data) => {
+        if (data != null) {
+          document.getElementById("name_header").innerHTML = data.name;
+          document.getElementById("info").innerHTML = data.info;
+          console.log(data);
+
+          data.education.forEach((element) => {
+            if (element != undefined) {
+              console.log(element);
+              let li = document.createElement("li");
+              li.innerText = element.name;
+              let span = document.createElement("span");
+              span.className = "time-span";
+              span.innerText = element.span;
+              li.appendChild(span);
+              document.getElementById("educations").appendChild(li);
+            }
+          });
+
+          data.workplaces.forEach((element) => {
+            if (element != undefined) {
+              console.log(element);
+              let li = document.createElement("li");
+              li.innerText = element.name;
+              let span = document.createElement("span");
+              span.className = "time-span";
+              span.innerText = element.span;
+              li.appendChild(span);
+              document.getElementById("workplaces").appendChild(li);
+            }
+          });
+        }
+      });
+  } catch (err) {
+    //console.log(err);
+    return null;
+  }
+}
